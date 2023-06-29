07b480b14b0b1713da50ddce7ff59d75b3323478
Sveta Piven <svetapiven93@icloud.com>
2023-06-29 19:51:53 +0300

    feat: added configuration in application.yml

--- src/main/java/com/trainlab/java_dev/JavaDevApplication.java
+++ src/main/java/com/trainlab/java_dev/JavaDevApplication.java
@@ -8,7 +8,7 @@ public class JavaDevApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JavaDevApplication.class, args);
-        System.out.println("Hello");
+        System.out.println("Hello world!");
     }
 
 }
