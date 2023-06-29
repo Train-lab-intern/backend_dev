6e97d8fe4fc126f94b8f413b1d000d6a09513d6a
Sveta Piven <svetapiven93@icloud.com>
2023-06-29 19:26:55 +0300

    feat: add dependencies

---
+++ src/main/java/com/trainlab/java_dev/JavaDevApplication.java
@@ -0,0 +1,14 @@
+package com.trainlab.java_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDevApplication.class, args);
+        System.out.println("Hello");
+    }
+
+}
