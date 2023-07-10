34bdf7edcf8a1bc41f066fb4d8a87a68e4303166
Sveta Piven <svetapiven93@icloud.com>
2023-07-10 20:06:09 +0300

    bug fix

--- api/src/main/java/com/trainlab/BackendDevApplication.java
+++ api/src/main/java/com/trainlab/BackendDevApplication.java
@@ -2,9 +2,11 @@ package com.trainlab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication(scanBasePackages = "com.trainlab")
+@PropertySource("classpath:/database.yml")
 @EnableWebMvc
 public class BackendDevApplication {
 
