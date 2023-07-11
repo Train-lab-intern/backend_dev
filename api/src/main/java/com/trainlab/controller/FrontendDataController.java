8b4ff3ab73ca93c80b7bc5926a82979a2eb3e8cf
Sveta Piven <svetapiven93@icloud.com>
2023-07-11 13:26:57 +0300

    feat: added new controller endpoint /front

--- api/src/main/java/com/trainlab/controller/FrontendDataController.java
+++ api/src/main/java/com/trainlab/controller/FrontendDataController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,11 +18,17 @@ import java.util.List;
 public class FrontendDataController {
     private final FrontendDataRepository frontendDataRepository;
 
-    @GetMapping("/{frontId}")
-    public ResponseEntity<FrontendData> getTextByFrontId(@PathVariable float frontId) {
-        FrontendData frontendData = frontendDataRepository.findByFrontId(frontId);
+    @GetMapping("/main-page")
+    public ResponseEntity<FrontendData> getMainPageData() {
+        List<FrontendData> mainPageDataList = frontendDataRepository.findMainPageData();
 
-        return new ResponseEntity<>(frontendData, HttpStatus.OK);
+        if (mainPageDataList.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        FrontendData mainPageData = mainPageDataList.get(0);
+
+        return new ResponseEntity<>(mainPageData, HttpStatus.OK);
     }
 
     @GetMapping
--- common/src/main/java/com/trainlab/repository/FrontendDataRepository.java
+++ common/src/main/java/com/trainlab/repository/FrontendDataRepository.java
@@ -2,7 +2,12 @@ package com.trainlab.repository;
 
 import com.trainlab.model.FrontendData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface FrontendDataRepository extends JpaRepository<FrontendData, Integer> {
     FrontendData findByFrontId(float frontId);
+
+    List<FrontendData> findMainPageData();
 }
