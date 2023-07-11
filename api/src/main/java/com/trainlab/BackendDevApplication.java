0dc6febfa85b1689070c46493ce19e76c14a92d2
Sveta Piven <svetapiven93@icloud.com>
2023-07-11 11:32:32 +0300

    bug fix

--- api/src/main/java/com/trainlab/BackendDevApplication.java
+++ api/src/main/java/com/trainlab/BackendDevApplication.java
@@ -2,22 +2,33 @@ package com.trainlab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication(scanBasePackages = "com.trainlab")
-//@PropertySource("classpath:database.yml")
 @EnableWebMvc
 public class BackendDevApplication {
 
     public static void main(String[] args) {
+
+        String dbHost = System.getenv("DB_HOST");
+        String dbPort = System.getenv("DB_PORT");
+        String dbName = System.getenv("DB_NAME");
+        String dbUsername = System.getenv("DB_USERNAME");
+        String dbPassword = System.getenv("DB_PASSWORD");
+
+        configureDataSource(dbHost, dbPort, dbName, dbUsername, dbPassword);
+
         SpringApplication.run(BackendDevApplication.class, args);
     }
 
-    String dbHost = System.getenv("DB_HOST");
-    String dbPort = System.getenv("DB_PORT");
-    String dbName = System.getenv("DB_NAME");
-    String dbUsername = System.getenv("DB_USERNAME");
-    String dbPassword = System.getenv("DB_PASSWORD");
+    private static void configureDataSource(String dbHost, String dbPort, String dbName, String dbUsername, String dbPassword) {
+        String jdbcUrl = "jdbc:postgresql://" + dbHost + ":" + dbPort + "/" + dbName;
 
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(jdbcUrl);
+        dataSource.setUsername(dbUsername);
+        dataSource.setPassword(dbPassword);
+    }
 }
