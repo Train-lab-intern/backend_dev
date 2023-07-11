abf7f5b5d8d17453e26deb6774a01c7933348757
Sveta Piven <svetapiven93@icloud.com>
2023-07-11 10:56:49 +0300

    bug fix

--- api/src/main/java/com/trainlab/BackendDevApplication.java
+++ api/src/main/java/com/trainlab/BackendDevApplication.java
@@ -6,12 +6,18 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication(scanBasePackages = "com.trainlab")
-@PropertySource("classpath:/database.yml")
+//@PropertySource("classpath:database.yml")
 @EnableWebMvc
 public class BackendDevApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendDevApplication.class, args);
     }
+
+    String dbHost = System.getenv("DB_HOST");
+    String dbPort = System.getenv("DB_PORT");
+    String dbName = System.getenv("DB_NAME");
+    String dbUsername = System.getenv("DB_USERNAME");
+    String dbPassword = System.getenv("DB_PASSWORD");
 
 }
