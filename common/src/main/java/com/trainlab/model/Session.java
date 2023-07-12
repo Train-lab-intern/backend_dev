3ab1daa174e8d3ed168275ed16891910e94fd752
Sveta Piven <svetapiven93@icloud.com>
2023-07-12 21:19:23 +0300

    feat: added session_table and entity

---
+++ common/src/main/java/com/trainlab/model/Session.java
@@ -0,0 +1,70 @@
+package com.trainlab.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.sql.Timestamp;
+import java.util.Collections;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "sessions")
+public class Session {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotNull
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @NotNull
+    @Column(name = "session_token", nullable = false, length = 256)
+    private String sessionToken;
+
+    @JsonIgnore
+    @NotNull
+    @Column
+    private Timestamp created;
+
+    @JsonIgnore
+    @NotNull
+    @Column
+    private Timestamp changed;
+
+    @JsonIgnore
+    @Column(name = "is_deleted")
+    private boolean isDeleted = false;
+
+    @JsonIgnore
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    @OneToMany(mappedBy = "id", cascade = {CascadeType.MERGE, CascadeType.REFRESH} , fetch = FetchType.LAZY)
+    @JsonManagedReference
+    private Set<User> users = Collections.emptySet();
+}
--- common/src/main/resources/db/migration/V1_1__initial_script.sql
+++ common/src/main/resources/db/migration/V1_1__initial_script.sql
@@ -1,14 +1,3 @@
-create table public.test
-(
-    id    serial
-        primary key
-        unique,
-    hello varchar
-);
-
-alter table public.test
-    owner to trainlab;
-
 create table frontend_data
 (
     id         serial
@@ -29,4 +18,20 @@ create index frontend_data_front_id_index
 
 alter table frontend_data
     add unique (front_id);
+
+create table public.sessions
+(
+    id            serial
+        primary key,
+    user_id       integer                    not null
+        unique,
+    session_token varchar(255)               not null
+        unique,
+    created       timestamp(6) default now() not null,
+    changed       timestamp(6)               not null,
+    is_deleted    boolean      default false not null
+);
+
+alter table public.sessions
+    owner to trainlab;
 
