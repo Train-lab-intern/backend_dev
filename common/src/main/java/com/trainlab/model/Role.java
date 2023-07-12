5f10a2ea4313f30bc62f67a4931ac5cefcb9423b
KnyazevaVlada <vladislava147@gmail.com>
2023-07-12 21:05:31 +0300

    created tables users and roles, and entities to these tables added scripts to V1_1__initial_script.sql

---
+++ common/src/main/java/com/trainlab/model/Role.java
@@ -0,0 +1,69 @@
+package com.trainlab.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.cache.annotation.Cacheable;
+
+import java.sql.Timestamp;
+import java.util.Collections;
+import java.util.Set;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Cacheable("roles")
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Size(max = 256)
+    @NotNull
+    @Column(name = "role_name", nullable = false, length = 256)
+    private String roleName;
+
+    @NotNull
+    @Column
+    @JsonIgnore
+    private Timestamp created;
+
+    @NotNull
+    @Column
+    @JsonIgnore
+    private Timestamp changed;
+
+    @NotNull
+    @Column
+    @JsonIgnore
+    private boolean isDeleted;
+
+    @JsonIgnore
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    @OneToMany(mappedBy = "id", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonManagedReference
+    private Set<User> users = Collections.emptySet();
+}
---
+++ common/src/main/java/com/trainlab/model/User.java
@@ -0,0 +1,74 @@
+package com.trainlab.model;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.AttributeOverrides;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.cache.annotation.Cacheable;
+
+import java.sql.Timestamp;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Cacheable("users")
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotNull
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "email", column = @Column(name = "email", nullable = false, length = 256)),
+            @AttributeOverride(name = "userPassword", column = @Column(name = "user_password", nullable = false, length = 256))
+    })
+    private AuthenticationInfo authenticationInfo;
+
+    @NotNull
+    @Column
+    @JsonIgnore
+    private Timestamp created;
+
+    @NotNull
+    @Column
+    @JsonIgnore
+    private Timestamp changed;
+
+    @NotNull
+    @Column(name = "is_deleted")
+    @JsonIgnore
+    private boolean isDeleted = false;
+
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "role_id")
+    @JsonIgnoreProperties({"users", "roleName", "created", "changed", "isDeleted"})
+    private Role roleId;
+}
