9f7584db8a78970f44e6bfffe0dac32406563b1b
Kate Komleva <komlevakate99@gmail.com>
2023-08-02 15:11:12 +0300

    feat: activated @ValidPassword.

--- common/src/main/java/com/trainlab/model/Session.java
+++ common/src/main/java/com/trainlab/model/Session.java
@@ -1,6 +1,5 @@
 package com.trainlab.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
--- common/src/main/java/com/trainlab/service/impl/UserServiceImpl.java
+++ common/src/main/java/com/trainlab/service/impl/UserServiceImpl.java
@@ -24,7 +24,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -105,7 +104,7 @@ public class UserServiceImpl implements UserService {
         return userRepository.save(updated);
     }
 
-    public void changePassword(UserUpdateRequest userUpdateRequest, Long id){
+    public void changePassword(UserUpdateRequest userUpdateRequest, Long id) {
         User user = userCheck(id);
         String emailSubject = "Вы забыли пароль";
         String toAddress = userUpdateRequest.getEmail();
