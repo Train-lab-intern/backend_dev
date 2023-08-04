8ccab4ac46ded9167b60af77b39771d3848124a7
Petrovich-A <A.Piatrovich@gmail.com>
2023-08-04 09:14:30 +0300

    :adhesive_bandage: minor fix for a non-critical issue

--- common/src/main/java/com/trainlab/mapper/RoleMapper.java
+++ common/src/main/java/com/trainlab/mapper/RoleMapper.java
@@ -2,10 +2,12 @@ package com.trainlab.mapper;
 
 import com.trainlab.dto.request.RoleRequest;
 import com.trainlab.model.Role;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
@@ -14,6 +16,7 @@ public interface RoleMapper {
     @Mapping(target = "changed", expression = "java(java.sql.Timestamp.valueOf(java.time.LocalDateTime.now()))")
     Role toEntity(RoleRequest roleRequest);
 
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     @Mapping(target = "changed", expression = "java(java.sql.Timestamp.valueOf(java.time.LocalDateTime.now()))")
     Role partialUpdateToEntity(RoleRequest roleRequest, @MappingTarget Role role);
 
