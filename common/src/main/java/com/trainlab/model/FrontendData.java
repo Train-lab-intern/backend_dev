f34f3cec2e76e5869defd48972998d168a943bc6
Petrovich-A <A.Piatrovich@gmail.com>
2023-08-18 10:19:30 +0300

    :white_check_mark: added test for FrontendData

--- common/src/main/java/com/trainlab/model/FrontendData.java
+++ common/src/main/java/com/trainlab/model/FrontendData.java
@@ -10,9 +10,11 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.sql.Timestamp;
 
@@ -23,6 +25,8 @@ import java.sql.Timestamp;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@EqualsAndHashCode
+@ToString
 public class FrontendData {
     @JsonIgnore
     @Id
