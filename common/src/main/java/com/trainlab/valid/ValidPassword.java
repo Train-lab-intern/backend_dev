cbce2e3b34afe81b43aba83ff6cf6879c9321910
Sveta Piven <svetapiven93@icloud.com>
2023-08-25 13:43:17 +0300

    refactor ValidPassword

--- common/src/main/java/com/trainlab/valid/ValidPassword.java
+++ common/src/main/java/com/trainlab/valid/ValidPassword.java
@@ -3,7 +3,6 @@ package com.trainlab.valid;
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 import jakarta.validation.ReportAsSingleViolation;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 import java.lang.annotation.Documented;
@@ -16,7 +15,7 @@ import java.lang.annotation.Target;
 @Constraint(validatedBy = {})
 @Target({ElementType.FIELD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
-@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?!.*[а-яА-Я]).{8,}$")
+@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?!.*[а-яА-Я]).{8,}$")
 @ReportAsSingleViolation
 public @interface ValidPassword {
     String message() default "Password must contain 8 characters and at least one lowercase and one uppercase character";
