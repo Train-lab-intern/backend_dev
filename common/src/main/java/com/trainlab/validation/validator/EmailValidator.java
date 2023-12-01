761e7eae24dc26c713cf3a4a6ec641710568e126
Vladislav_Global <vladalexkirbut2003@gmail.com>
2023-12-01 16:09:32 +0300

    Merge with email-fix branch

--- common/src/main/java/com/trainlab/validation/validator/EmailValidator.java
+++ common/src/main/java/com/trainlab/validation/validator/EmailValidator.java
@@ -5,14 +5,13 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import org.springframework.stereotype.Component;
 
-import java.util.regex.Pattern;
-
 @Component
 public class EmailValidator implements ConstraintValidator<Email, String> {
     private final static String IPv4_PATTERN = "\\[((25[0-5]|2[0-4]\\d|[01]?\\d?\\d)(\\.)){3}(25[0-5]|2[0-4]\\d|[01]?\\d?\\d)]";
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
-        return value.matches("^[A-Za-z0-9._%+\\-]+@(" + IPv4_PATTERN +  "|([A-Za-z0-9.-]+\\.[A-Za-z]{2,4}))$");
+        return value.matches("^[A-Za-z0-9._%+\\-]+@(" + IPv4_PATTERN +
+                "|([A-Za-z0-9.-]+\\.[A-Za-z]{2,4}))$");
     }
 }
