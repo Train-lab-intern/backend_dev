da59bfe408c50d102b7791975fff71adde905f57
Vladislav Kirbut <88002952+VladislavKirbut@users.noreply.github.com>
2023-12-07 11:46:01 +0300

    Update EmailValidator.java

--- common/src/main/java/com/trainlab/validation/validator/EmailValidator.java
+++ common/src/main/java/com/trainlab/validation/validator/EmailValidator.java
@@ -13,6 +13,6 @@ public class EmailValidator implements ConstraintValidator<Email, String> {
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
-        return value.matches("^[A-Za-z0-9._%+\\-]+@(" + IPv4_PATTERN +  "|([A-Za-z0-9.-]+\\.[A-Za-z]{2,4}))$");
+        return value.matches("^[A-Za-z0-9._%+\\-'`]+@(" + IPv4_PATTERN +  "|([A-Za-z0-9.-]+\\.[A-Za-z]{2,4}))$");
     }
 }
