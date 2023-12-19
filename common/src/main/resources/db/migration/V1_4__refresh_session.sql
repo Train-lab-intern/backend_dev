e2b2c3f1aee74815202414fbf4914ea3fdf082c6
Pavel Naibich <npo16032005@gmail.com>
2023-12-19 21:35:59 +0300

    v1_4 migr

--- common/src/main/resources/db/migration/V1_4__refresh_session.sql
+++ common/src/main/resources/db/migration/V1_4__refresh_session.sql
@@ -1,6 +1,6 @@
--- CREATE TABLE refresh_sessions (
---   id BIGSERIAL PRIMARY KEY,
---   user_id BIGINT REFERENCES users(id) ON DELETE CASCADE,
---   refresh_token UUID NOT NULL,
---   expired_at TIMESTAMP(6) NOT NULL,
---   issued_at TIMESTAMP(6) NOT NULL);
+CREATE TABLE refresh_sessions (
+  id BIGSERIAL PRIMARY KEY,
+  user_id BIGINT REFERENCES users(id) ON DELETE CASCADE,
+  refresh_token UUID NOT NULL,
+  expired_at TIMESTAMP(6) NOT NULL,
+  issued_at TIMESTAMP(6) NOT NULL);
