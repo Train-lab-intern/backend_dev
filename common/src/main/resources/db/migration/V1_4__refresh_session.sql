99163f1ac968ee28f67bb55234d90a9a3b62c256
Pavel Naibich <npo16032005@gmail.com>
2023-12-19 21:07:13 +0300

    migration update

--- common/src/main/resources/db/migration/V1_4__refresh_session.sql
+++ common/src/main/resources/db/migration/V1_4__refresh_session.sql
@@ -3,5 +3,4 @@ CREATE TABLE refresh_sessions (
   user_id BIGINT REFERENCES users(id) ON DELETE CASCADE,
   refresh_token UUID NOT NULL,
   expired_at TIMESTAMP(6) NOT NULL,
-  issued_at TIMESTAMP(6) NOT NULL DEFAULT
-);
+  issued_at TIMESTAMP(6) NOT NULL);
