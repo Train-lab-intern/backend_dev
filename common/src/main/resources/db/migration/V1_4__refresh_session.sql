a02c73f0e0151a10d6af001a9944179a14ba0e6e
Vladislav Kirbut <88002952+VladislavKirbut@users.noreply.github.com>
2023-12-19 19:23:43 +0300

    Update V1_4__refresh_session.sql

--- common/src/main/resources/db/migration/V1_4__refresh_session.sql
+++ common/src/main/resources/db/migration/V1_4__refresh_session.sql
@@ -1,1 +1,7 @@
-DROP TABLE refresh_sessions;
+CREATE TABLE refresh_sessions (
+  id BIGSERIAL PRIMARY KEY,
+  user_id BIGINT REFERENCES users(id) ON DELETE CASCADE,
+  refresh_token UUID NOT NULL,
+  expired_at TIMESTAMP(6) NOT NULL,
+  issued_at TIMESTAMP(6) NOT NULL DEFAULT
+);
