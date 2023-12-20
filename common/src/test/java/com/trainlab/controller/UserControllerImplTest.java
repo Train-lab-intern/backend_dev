26833daae476c39c885450bddf15b5447df553e4
Vladislav_Global <vladalexkirbut2003@gmail.com>
2023-12-21 00:42:09 +0300

    Prepared class for userController testing

---
+++ common/src/test/java/com/trainlab/controller/UserControllerImplTest.java
@@ -0,0 +1,8 @@
+package com.trainlab.controller;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(value = MockitoJUnitRunner.class)
+public class UserControllerImplTest {
+}
